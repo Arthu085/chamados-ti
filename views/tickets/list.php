8f494df48e27f89fed25607f92eeaae336d90aff
Arthur Ghizi <arthurghizi22@gmail.com>
2025-05-22 15:45:38 -0300

    tela de listagem de chamado em geral finalizada

---
+++ views/tickets/list.php
@@ -0,0 +1,35 @@
+<?php
+require_once '../includes/auth.php';
+$user = checkAuth();
+?>
+
+<?php include '../includes/header.php'; ?>
+
+<button class="btn btn-outline-dark d-lg-none m-2" type="button" data-bs-toggle="offcanvas"
+    data-bs-target="#sidebarMenu">
+    <img src="/CHAMADOS-TI/assets/img/burger-menu-svgrepo-com.svg" width="24" class="menu-icon" alt="Menu">
+</button>
+
+<main class="container-fluid">
+    <div class="row">
+        <?php include '../includes/sidebar.php'; ?>
+
+        <div class="col-lg-10 ms-auto p-4">
+            <h2>Lista de Chamados</h2>
+            <p>Visualização de todos os chamados cadastrados.</p>
+            <div class="card border-secondary">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>Lista</span>
+                    <a href="new.php" title="Novo chamado" class="btn btn-primary">Abrir chamado</a>
+                </div>
+                <div id="tickets-list" class="card-body"></div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</main>
+
+<script type="module" src="/CHAMADOS-TI/assets/js/api/ticketApi.js"></script>
+
+<?php include '../includes/footer.php'; ?>
