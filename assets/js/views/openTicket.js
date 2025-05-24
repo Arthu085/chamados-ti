e3f051e0cbc7f0fd554ce409a5b1d9c2c1b8c776
Arthu085 <arthurghizi22@gmail.com>
2025-05-24 12:35:20 -0300

    removido log de adicionar contato na criação do chamado

--- assets/js/views/openTicket.js
+++ assets/js/views/openTicket.js
@@ -44,12 +44,6 @@ $(document).ready(function () {
 			.map(function () {
 				const rawPhone = $(this).find(".contact-phone").val();
 				const cleanPhone = rawPhone.replace(/\D/g, "");
-				console.log(
-					"Telefone original:",
-					rawPhone,
-					"Telefone limpo:",
-					cleanPhone
-				);
 				return {
 					name: $(this).find(".contact-name").val(),
 					phone: cleanPhone,
